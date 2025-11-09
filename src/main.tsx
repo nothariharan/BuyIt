3f849bf61d953fd5be6619e018058dd4bee55727
nothariharan <nothariharan@gmail.com>
2025-11-09 09:15:46 +0530

    did some debugging filter part is now done

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { FilterProvider } from './components/FilterContent.tsx'
+import { FilterProvider } from './components/FilterContext.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FilterProvider>
